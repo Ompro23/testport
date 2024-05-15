9e75ccbc21eed71797bbb259dda18187c7557ab4
Om vataliya <omcodesprojects@gmail.com>
2024-05-16 01:25:35 +0530

    intial commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const base = '/portfolio';
@@ -30,3 +30,5 @@ const config = {
 };
 
 export default config;
+
+
